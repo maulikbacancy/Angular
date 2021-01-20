4ffa074e2475606777ea7238f2bb4924c054c1f1
Maulik Gevariya <maulikgevariya@users.noreply.github.com>
2021-01-20 19:25:03 +0530

    mobile responsive

--- Imvitium/src/app/app-routing.module.ts
+++ Imvitium/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './core/about/about.component';
 import { LoginComponent } from './core/auth/login/login.component';
 import { SignupComponent } from './core/auth/signup/signup.component';
 import { ContactComponent } from './core/contact/contact.component';
+import { FeaturesComponent } from './core/features/features.component';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'features', component: FeaturesComponent},
 ];
 
 @NgModule({
