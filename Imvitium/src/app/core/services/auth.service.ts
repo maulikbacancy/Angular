948becac3d571f88f49dcdd06d8a7cc0f9094fde
Maulik Gevariya <maulikgevariya@users.noreply.github.com>
2021-03-24 12:16:23 +0530

    relative path update

--- Imvitium/src/app/core/services/auth.service.ts
+++ Imvitium/src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { LoginUserModel } from '../models/login-user.model';
 import { SignUpUserModel } from '../models/signup-user.model';
 import { UserModel } from '../models/user.model';
